fd29a1b236bbf2dc15ccef50219ac813e1a862e7
Chris-Verichon <chris.verichon@gmail.com>
2022-08-02 15:48:59 +0200

    CoreModel for centralize properties and methods useful for all model

---
+++ bin/CoreModel.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace REPLACENAMESPACE\Models;
+
+// Mother class of all Models
+// We centralize here all the properties and methods useful for ALL the Models
+class CoreModel {
+    /**
+     * @var int
+     */
+    protected $id;
+    /**
+     * @var string
+     */
+    protected $created_at;
+    /**
+     * @var string
+     */
+    protected $updated_at;
+
+
+    /**
+     * Get the value of id
+     *
+     * @return  int
+     */ 
+    public function getId() : int
+    {
+        return $this->id;
+    }
+
+    /**
+     * Get the value of created_at
+     *
+     * @return  string
+     */ 
+    public function getCreatedAt() : string
+    {
+        return $this->created_at;
+    }
+
+    /**
+     * Get the value of updated_at
+     *
+     * @return  string
+     */ 
+    public function getUpdatedAt() : string
+    {
+        return $this->updated_at;
+    }
+}
